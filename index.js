626ce2f47402fbb088f47b6bd274516d3b0b7169
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-11-02 18:18:55 +0100

    making the band

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-<!-- 2014 I went to Africa baby-->
+// 2014 I went to Africa baby
+// get functions/ get function call/ get interactivity/ get event listeners/ get api premissions/ get xss/ get php/ get sql/ upload/
+
+function dilemmas(fever,simaska) {
+
+if (fever =>) :
+
+addEventListener
+}
