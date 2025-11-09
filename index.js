aa3f41d138a958fb57d8d6793a741952fddcf743
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-11-09 16:57:26 +0100

    11/09/2025 16:57:26

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
-// 2014 I went to Africa baby
-// get functions/ get function call/ get interactivity/ get event listeners/ get api premissions/ get xss/ get php/ get sql/ upload/
-//man npm; lcommit in npm;
-//obsolete js; obfuscated js; deobfuscate k;
+/*  2014 I went to Africa baby: cap;never left it; sebtyni le al hozn al Nabil
+get function call/ get interactivity/ get event listeners/ get api premissions/ get xss/ get php/ get sql/ upload/ man npm; lcommit in npm;obsolete js; obfuscated js; deobfuscate k;?2 values for onclick
+?inetsynced animations;
+?hidden items: animation> glep tv, dim bg 1sec during; Onclick: A:glep.LRR, B:glep.RTL; C:khaby.LTR&RTL;
+dim the button: spam prevention; click conunt and act accordingly
 
 
-/*
-?2 values for onclick
-checker for animations align with another animations
-animations for hidden items aka gelp whatching the tv and going wild with it;
-view port for mobile bot good;
-get font and whatnot
-multiple versions for page for diffferent devices;
-sizes relivant to device width and height;
-is with percentage?
+N8N CI/CD:
+A:fetch gh commits: linkedin & portfolio 
+B:fetch linkedin posts for a webpage
 
-
-
-function dilemmas(fever,simaska) {
-
-if (fever =>) :
-
-addEventListener
-
-sebtyni le al hozn al Nabil
-}
+??:
+?add capti
+?multiple versions for page for diffferent devices;  ?view port for mobile;sizes relivant to device width and height;?size percentage?
+?emojies
 */
 
-/*let makemecv = pdfmaker()
+/*
+function dilemmas(fever,simaska)
+{if (fever =>) :
+addEventListener
+}
+
+let makemecv = pdfmaker()
 import lablemake from "lablemake";
 let pdfmaker =
-
-
-
   document.getElementByID.(live)
-
-
 */
 
 function livemelive()
