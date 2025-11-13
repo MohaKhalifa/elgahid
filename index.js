1cc52794413adb36c16ccaf2c08846e71a1b46fb
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-11-13 01:58:34 +0100

    11/13/2025 01:58:34

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-/*  2014 I went to Africa baby: cap;never left it; sebtyni le al hozn al Nabil
-get function call/ get interactivity/ get event listeners/ get api premissions/ get xss/ get php/ get sql/ upload/ man npm; lcommit in npm;obsolete js; obfuscated js; deobfuscate k;?2 values for onclick
+/* get function call/ get interactivity/ get event listeners/ get api premissions/ get xss/ get php/ get sql/ upload/ man npm; lcommit in npm;obsolete js; obfuscated js; deobfuscate k;?2 values for onclick
 ?inetsynced animations;
 ?hidden items: animation> glep tv, dim bg 1sec during; Onclick: A:glep.LRR, B:glep.RTL; C:khaby.LTR&RTL;
 dim the button: spam prevention; click conunt and act accordingly
-
-
 N8N CI/CD:
 A:fetch gh commits: linkedin & portfolio 
 B:fetch linkedin posts for a webpage
@@ -13,6 +10,7 @@ B:fetch linkedin posts for a webpage
 ?add capti
 ?multiple versions for page for diffferent devices;  ?view port for mobile;sizes relivant to device width and height;?size percentage?
 ?emojies
+Object.addEventListener ("", function)
 */
 
 /*
@@ -45,3 +43,16 @@ function liver(){
 
 
 
+document.addEventListener(Key("d"),liva());
+
+
+function liva() {
+let x = document.getElementsByClassName("");
+let siva = x/4;
+return siva;
+
+
+}
+
+
+document.addEventListener()
