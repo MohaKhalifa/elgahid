09a2c6069e695e79fed47f47252689a633c0760d
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-12-04 20:59:21 +0100

    12/04/2025 20:59:21

--- index.js
+++ index.js
@@ -1,10 +1,84 @@
-/* get function call/ get interactivity/ get event listeners/ get api premissions/ get xss/ get php/ get sql/ upload/ man npm; lcommit in npm;obsolete js; obfuscated js; deobfuscate k;?2 values for onclick
+/*
+JS jargon;
+ASI: automatic semi-colon insertion:
+
+Declirations in js:
+no longer in use; Var: block-level scoping; inside the whole function
+Let only available within the block {}; can be reassigned not redeclared;
+Const: block level scoping cannot be changed? cannot be reassigned or redeclared within the same blcok; xss?
+
+composite datatypes:can be changed during execution
+array: nig = [3,3,3,2,3];?call
+Object: siva {age: "", fjdk:""}
+Operators:
+  Arethmatic:Math:
+  Comparison: compair and return True or false;
+  != inequal; == equal, ===equal with the same datatype;
+  logical: man the return of cpmparison: &&: ||: !x;
+call value form a var ${}
+
+
+
+
+
+get function call/ get interactivity/ get event listeners/ get api premissions/ get xss/ get php/ get sql/ upload/ man npm; lcommit in npm;obsolete js; obfuscated js; deobfuscate k;?2 values for onclick
 ?inetsynced animations;
 ?hidden items: animation> glep tv, dim bg 1sec during; Onclick: A:glep.LRR, B:glep.RTL; C:khaby.LTR&RTL;
 dim the button: spam prevention; click conunt and act accordingly
 N8N CI/CD:
 A:fetch gh commits: linkedin & portfolio 
 B:fetch linkedin posts for a webpage
+
+JS usie cases; purpose; interativity
+
+js basic syntax;
+built-InFunctions("");
+js built in functions:
+  pop-Ups in js:
+  alert(""); message;
+  confirm("message"); returns a boolina y/n;
+  Prombt("message"); user input prombt;
+
+
+
+DOM Man:
+document.getELementById("live").innerHtml = "siva"; changes the id values to siva;
+  get the css;
+
+
+DOM man;
+Handeling events
+from validation;
+feedback; aka responsive forms
+reaload the feed
+visual effects
+
+
+SDLC IN js:
+ use gest and mocha to make the tests
+ async: await; run while
+ functions for predicatble code
+es6 the js syntax; constantly updatedj;
+try catfch block for error handeling; chrome dev tools; console;
+js for front end;
+  Reach
+  Vue
+  Angular
+
+  Express for backend
+  runtime environment:
+  Node.js
+
+Mocha or Jasmine for testing before the code is even written
+
+Security: 
+xss; csrm;
+
+Maintainance; documentation;
+JSDOC; doc
+
+
+
 
 ??:
 ?add capti
@@ -25,34 +99,25 @@ let pdfmaker =
   document.getElementByID.(live)
 */
 
-function livemelive()
- {
-    
-    
-   alert("heyNow");
-}
 function changer() {
-
-
   document.getElementById('dates').style.color = "red";
 }
-function liver(){
 
-  document.querySelector(body).css.color = "invert 1";
+
+/*looping: do {} while () exec once and then checks the con: if t exec again;  */
+
+ function sarika()
+  {
+    for ( let si = 33; si > 45; si++)
+    {
+    console.log(si);
+   /* 
+while (si == 44)
+{
+  console.log("ok")
+}
+*/
+ }
 }
 
 
-
-document.addEventListener(Key("d"),liva());
-
-
-function liva() {
-let x = document.getElementsByClassName("");
-let siva = x/4;
-return siva;
-
-
-}
-
-
-document.addEventListener()
